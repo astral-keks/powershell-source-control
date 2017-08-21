f20bf629ab23f7fe8b495f7b626cad4f15b81cc6
astral-keks <astralkeks@gmail.com>
2017-08-22 02:09:07 +0300

    - Proper binaries versioning

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.2
+### Fixed
+- Proper binaries versioning
+
 ## 0.1.1
 ### Changed
 - AstralKeks.Workbench.PowerShell -> AstralKeks.PowerShell.Common (NuGet package)
--- build.ps1
+++ build.ps1
@@ -1,9 +1,16 @@
+$version = "0.1.2"
+
 $src = "$PSScriptRoot\src"
 $package = "$PSScriptRoot\package"
 $artifact = "$PSScriptRoot\artifact\AstralKeks.SourceControl"
+
+$manifest = "$artifact\AstralKeks.SourceControl.psd1"
 
 # Build binaries
 if (Test-Path $artifact) { Remove-Item $artifact -Recurse }
 if (Test-Path $package) { Remove-Item $package -Recurse }
 dotnet restore $src\Command\SourceControl.Command.csproj
-dotnet build $src\Command\SourceControl.Command.csproj --configuration Release -o $artifact
+dotnet build $src\Command\SourceControl.Command.csproj --configuration Release -o $artifact --version-suffix $version
+
+# Fix manifest version
+(Get-Content $manifest).replace("ModuleVersion = '0.0.1'", "ModuleVersion = '$version'") | Set-Content $manifest
